fd027c73c5a591ec732d7bc9b7395e3a231ed69b
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-03-12 20:22:41 -0400

    Creé la carpeta Models con su clase Prioridades

---
+++ Library/Models/Prioridades.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+        [Required(ErrorMessage = "Este campo Obligatorio")]
+        public string? Descripcion { get; set; }
+        [Range(1, 31, ErrorMessage = "Valores Validos entre 1 y 31")]
+        public int DiasCompromiso { get; set; }
+    }
+}
