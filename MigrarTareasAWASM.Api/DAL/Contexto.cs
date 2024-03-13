551b0319d7a2d91d2d4386dedc9f431f7733dee8
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-03-12 20:28:33 -0400

    Creé la carpeta DAL con su clase Contexto

---
+++ MigrarTareasAWASM.Api/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Library.Models;
+
+namespace MigrarTareasAWASM.Api.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prioridades> Prioridades { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Sistemas> Sistema { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
