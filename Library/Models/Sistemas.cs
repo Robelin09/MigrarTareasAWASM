f7f81e34ba23014be2c30e16838c131831a06311
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-03-12 20:23:19 -0400

    Creé la clase Sistemas en la carpeta Models

---
+++ Library/Models/Sistemas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "El Nombre del sistema es obligatorio.")]
+        public string? NombredeSistema { get; set; }
+    }
+}
