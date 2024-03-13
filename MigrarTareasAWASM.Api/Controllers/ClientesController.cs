408a84f4eb11e8eff21632eba3111bfffdc75ca0
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-03-12 22:20:35 -0400

    Crepe el ClientesController

---
+++ MigrarTareasAWASM.Api/Controllers/ClientesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Library.Models;
+using MigrarTareasAWASM.Api.DAL;
+
+namespace MigrarTareasAWASM.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public ClientesController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clientes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Clientes>>> GetClientes()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+
+        // GET: api/Clientes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Clientes>> GetClientes(int id)
+        {
+            var clientes = await _context.Clientes.FindAsync(id);
+
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+
+            return clientes;
+        }
+
+        // PUT: api/Clientes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClientes(int id, Clientes clientes)
+        {
+            if (id != clientes.ClienteId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(clientes).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clientes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Clientes>> PostClientes(Clientes clientes)
+        {
+            if (clientes.ClienteId <= 0 || !ClientesExists(clientes.ClienteId))
+            {
+                _context.Clientes.Add(clientes);
+            }
+            else
+            {
+                _context.Clientes.Update(clientes);
+            }
+            await _context.SaveChangesAsync();
+            return Ok(clientes);
+        }
+
+        // DELETE: api/Clientes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClientes(int id)
+        {
+            if (!_context.Clientes.Any())
+            {
+                return NotFound();
+            }
+            var clientes = await _context.Clientes.FindAsync(id);
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+            _context.Clientes.Remove(clientes);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // DELETE: api/Clientes
+        [HttpDelete("DeleteDetail/{id}")]
+        public async Task<IActionResult> DeleteDetail(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            var detail = await _context.Clientes.FirstOrDefaultAsync(c => c.ClienteId == id);
+            if (detail == null)
+            {
+                return NotFound();
+            }
+            _context.Clientes.Remove(detail);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        private bool ClientesExists(int id)
+        {
+            return _context.Clientes.Any(c => c.ClienteId == id);
+        }
+    }
+}
